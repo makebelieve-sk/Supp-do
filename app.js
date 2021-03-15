17ce7490023d6bfbd1025d04c1b74b7ad0e6fc07
alexey_skriabin <as@itprom.org>
2021-03-15 12:33:39 +0500

    added cors

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const config = require("./config/default.json");
 const mongoose = require("mongoose");
 const fileUpload = require("express-fileupload");
 const path = require("path");
+const cors = require("cors");
 
 const app = express();
 
@@ -33,6 +34,17 @@ if (process.env.NODE_ENV === "production") {
         res.sendFile(path.join(__dirname + "../client/build/index.html"));
     })
 }
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
+    res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+    );
+    next();
+});
+app.options("*", cors());
 
 async function start() {
     try {
