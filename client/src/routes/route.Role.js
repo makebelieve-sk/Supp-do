d6a3dee91ef5c63c763206da49611bc5b6c3c610
alexey_skriabin <as@itprom.org>
2021-06-10 09:54:29 +0500

    Добавление импорта переменной localStorage

--- client/src/routes/route.Role.js
+++ client/src/routes/route.Role.js
@@ -8,6 +8,7 @@ import {request} from "../helpers/functions/general.functions/request.helper";
 import {compareObjects} from "../helpers/functions/general.functions/compare";
 import {NoticeError, storeRole} from "./helper";
 import onRemove from "../helpers/functions/general.functions/removeTab";
+import {StorageVars} from "../options/global.options";
 
 export const RoleRoute = {
     // Адрес для работы с разделом "Роли"
